b8f598aa144fa144dd47fba96b2e4e23998d75fc
iPetrik <igarek200706@gmail.com>
2023-02-27 15:16:28 +0300

    router test

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import "./App.css";
 import { Provider } from "react-redux";
 import Widget from "./components/widget.jsx";
 import store from "./store/store.js";
+import {Routes, Route, Link } from "react-router-dom"
+import {HelpPage} from "./components/help.jsx"
 
 
 function App() {
   return (
     <Provider store={store}>
+      <header>
+        <Link to="/help">Help</Link>
+      </header>
       <div className="wrapper">
         <Widget />
       </div>
+      <Routes>
+        <Route path="/help" element={<HelpPage/>}/>
+      </Routes>
     </Provider>
   );
 }
---
+++ src/components/help.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HelpPage = () => {
+    return (
+        <div>
+            This page doesnt exist <Link to='/'>help</Link>
+        </div>
+    )
+}
+
+export default HelpPage
